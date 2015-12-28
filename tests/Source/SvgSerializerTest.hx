d88fbe5f717025257f97014a041ea03eaf5d8b1f
Alexandr Dem'yanenko <demjanenko@gmail.com>
2015-12-29 01:18:39 +0300

    utf-8 string serialization

--- tests/Source/SvgSerializerTest.hx
+++ tests/Source/SvgSerializerTest.hx
@@ -1,3 +1,4 @@
+import haxe.Utf8;
 import lib.ha.svg.gradients.SVGGradient.SVGStop;
 import lib.ha.svg.SVGData;
 import vectorx.svg.SvgSerializer;
@@ -5,43 +6,23 @@ import types.Data;
 
 class SvgSerializerTest extends unittest.TestCase
 {
-    public function testBasic(): Void
+    public function testUtf(): Void
     {
-        testDecode();
+        var srcString = "абвгдеёжзийклмнопрстуфхчцэюя";
+        var utf = SvgSerializer.utf8Encode(srcString);
+        assertEquals(srcString, SvgSerializer.utf8Decode(utf));
+    }
 
+    public function testBasicString(): Void
+    {
         var srcString = "абвгдеёжзийклмнопрстуфхчцэюя";
         var data: Data = new Data(1024);
-
-        var utf = SvgSerializer.utf8Encode(srcString);
-        var dbg: Array<Int> = [];
-        for (i in 0 ... utf.length)
-        {
-            dbg.push(utf.charCodeAt(i));
-        }
-        trace('[${dbg.join(",")}]');
-        trace(utf);
-
-        trace(SvgSerializer.utf8Decode(utf));
 
         SvgSerializer.writeString(data, srcString);
 
         data.offset = 0;
         var dstString = SvgSerializer.readString(data);
 
-        trace('dst: $dstString');
-        assertTrue(srcString == dstString);
-    }
-
-    private function testDecode(): Void
-    {
-        var buf: StringBuf = new StringBuf();
-        var dbgCodes: Array<Int> = [208,176,208,177,208,178,208,179,208,180,208,181,209,145,208,182,208,183,208,184,208,185,208,186,208,187,208,188,208,189,208,190,208,191,209,128,209,129,209,130,209,131,209,132,209,133,209,135,209,134,209,141,209,142,209,143];
-
-        for (i in 0 ... dbgCodes.length)
-        {
-            buf.addChar(dbgCodes[i]);
-        }
-
-        trace(buf);
+        assertEquals(srcString, dstString);
     }
 }
--- vectorx/svg/SvgSerializer.hx
+++ vectorx/svg/SvgSerializer.hx
@@ -1,5 +1,6 @@
 package vectorx.svg;
 
+import haxe.Utf8;
 import lib.ha.core.math.Calc;
 import haxe.Utf8;
 import lib.ha.svg.gradients.SVGGradient;
@@ -173,36 +174,22 @@ class SvgSerializer
 
     public static function writeString(data: Data, value: String): Void
     {
-/*        if (value.length > 0xfffe)
+        if (value.length > 0xfffe)
         {
             throw "String is too long";
         }
 
-        var isUtf8: Bool = isUtf8String(value);
+        var encoded: String = utf8Encode(value);
 
-        data.writeUInt8(isUtf8String ? 1 : 0);
+        data.writeUInt16(encoded.length);
         data.offset += 2;
 
-        data.writeUInt16(value.length);
-        data.offset += 2;
-
-        if (isUtf8String)
+        for (i in 0 ... encoded.length)
         {
-            for (i in 0 ... value.length)
-            {
-                var char: Int = value.charCodeAt(i);
-                trace('[$i] = $char');
-                data.writeUInt8(char);
-                data.offset++;
-            }
+            var char: Int = encoded.charCodeAt(i);
+            data.writeUInt8(char);
+            data.offset++;
         }
-        else
-        {
-
-        }
-
-        //DataStringTools.writeString(data, utf8String);
-        data.offset += utf8String.length;*/
     }
 
     public static function readString(data: Data): String
@@ -210,21 +197,17 @@ class SvgSerializer
         var len = data.readInt16();
         data.offset += 2;
 
-        trace('len: $len');
-
         var buf: StringBuf = new StringBuf();
 
         for (i in 0 ... len)
         {
-            trace(i);
             var char: UInt = data.readUInt8();
-            trace('char $char');
             data.offset++;
 
             buf.addChar(char);
         }
 
-        return buf.toString();
+        return utf8Decode(buf.toString());
     }
 
     /*public static function readGradient(data: Data, value: SVGGradient): SVGGradient
