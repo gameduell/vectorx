1250e167737c2f1d6e51e9c3fb7e09a8400f0c5f
Alexandr Dem'yanenko <demjanenko@gmail.com>
2016-01-19 00:30:22 +0300

    c# port wip

--- csharp/types/Data.hx
+++ csharp/types/Data.hx
@@ -27,6 +27,11 @@
 package types;
 
 import types.DataType;
+//System.Collections.Generic
+import cs.system.io.MemoryStream;
+import cs.system.io.BinaryReader;
+import cs.system.io.BinaryWriter;
+import cs.system.io.SeekOrigin;
 
 class Data
 {
@@ -39,30 +44,56 @@ class Data
     inline static public var SIZE_OF_FLOAT32: Int = 4;
     inline static public var SIZE_OF_FLOAT64: Int = 8;
 
+    var memory: MemoryStream;
+    var reader: BinaryReader;
+    var writer: BinaryWriter;
+
     public function new(sizeInBytes: Int): Void /// if 0, empty data, does not create the underlying memory. Can be set externally.
     {
-
+        allocedLength = sizeInBytes;
+        memory = new MemoryStream(sizeInBytes);
+        reader = new BinaryReader(memory);
+        writer = new BinaryWriter(memory);
     }
 
     public function writeData(data: Data): Void
     {
-
+        data.memory.Seek(0, SeekOrigin.Begin);
+        memory.Seek(0, SeekOrigin.Begin);
+        data.memory.WriteTo(memory);
     }
 
 // Int write and read functions
 
     public function writeInt(value: Int, targetDataType: DataType): Void
     {
-
+        switch(targetDataType)
+        {
+            case DataType.DataTypeInt8: writeInt8(value);
+            case DataType.DataTypeUInt8: writeUInt8(value);
+            case DataType.DataTypeInt16: writeInt16(value);
+            case DataType.DataTypeUInt16: writeUInt16(value);
+            case DataType.DataTypeInt32: writeInt32(value);
+            case DataType.DataTypeUInt32: writeUInt32(value);
+            case DataType.DataTypeFloat32: writeFloat32(value);
+            case DataType.DataTypeFloat64: writeFloat64(value);
+        }
     }
+
     public function writeIntArray(array: Array<Int>, dataType: DataType): Void
     {
 
     }
 
+    private inline function seek(): Void
+    {
+        memory.Seek(offset, SeekOrigin.Begin);
+    }
     public function writeInt8(value: Int): Void
     {
-
+        seek();
+        var tmp: cs.types.Int8 = (value);
+        writer.Write(tmp);
     }
 
     public function writeUInt8(value: Int): Void
@@ -92,7 +123,17 @@ class Data
 
     public function readInt(targetDataType: DataType): Int
     {
-        return 0;
+        switch(targetDataType)
+        {
+            case DataType.DataTypeInt8: return readInt8();
+            case DataType.DataTypeUInt8: return readUInt8();
+            case DataType.DataTypeInt16: return readInt16();
+            case DataType.DataTypeUInt16: return readUInt16();
+            case DataType.DataTypeInt32: return readInt32();
+            case DataType.DataTypeUInt32: return readUInt32();
+            case DataType.DataTypeFloat32: return Std.int(readFloat32());
+            case DataType.DataTypeFloat64: return Std.int(readFloat64());
+        }
     }
 
     public function readIntArray(count: Int, dataType: DataType): Array<Int>
@@ -182,7 +223,8 @@ class Data
     public var offsetLength: Int;
     public function resetOffset(): Void /// makes offset 0 and offsetLength be allocedLength
     {
-
+        offset = 0;
+        offsetLength = allocedLength;
     }
 
 /// should not be used for reading and writing on the data
@@ -191,7 +233,8 @@ class Data
 /// if underlying pointer is set externally a new pointer will be created with a copy of that external pointer's memory.
     public function resize(newSize: Int): Void
     {
-
+        memory.SetLength(newSize);
+        allocedLength = newSize;
     }
 
 /// makes the part pointed by offset and offset length become the full length of the data
