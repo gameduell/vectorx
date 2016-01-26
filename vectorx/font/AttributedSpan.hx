e48dbaf784bfc0c9037a2f025aa485b5f7de09e1
Alexandr Dem'yanenko <demjanenko@gmail.com>
2016-01-26 18:17:58 +0300

    debug helper

--- vectorx/font/AttributedSpan.hx
+++ vectorx/font/AttributedSpan.hx
@@ -71,7 +71,7 @@ class AttributedSpan
     public function toString(): String
     {
         //return '{id: $id, range: ${range.index}[${range.length}] str: $string font: $font backgroud: $backgroundColor measure: {x: ${measure.x} y: ${measure.y}} attachment: $attachment}';
-        return '{id: $id, range: ${range.index}[${range.length}] str: $string kern: $kern font: $font}';
+        return '{id: $id, range: ${range.index}[${range.length}] str: $string font: $font attachment: $attachmentId}';
     }
 
     private inline function choose<T>(dst: T, src: T)
